ba1b96ad248b1dc24efe38916c534c70b7f04c50
kassrojas <kassandrarojass@gmail.com>
2023-01-23 16:11:52 -0500

    added some styling changes

--- public/js/room.js
+++ public/js/room.js
@@ -19,13 +19,14 @@ const submitRoom = async (event) => {
     if (roomName) {
         const response = await fetch('/api/rooms', {
             method: 'POST',
-            body: JSON.stringify({ 
+            body: JSON.stringify({
                 name: roomName,
                 dashed_name: dashedName
             }),
             headers: { 'Content-Type': 'application/json' },
         }
-    )};
+        )
+    };
 
     const here = document.location;
     document.location.replace(`${here}`);
@@ -34,7 +35,7 @@ const submitRoom = async (event) => {
 const updateRoom = async (event) => {
     event.preventDefault();
     console.log('hello')
-    
+
     const roomName = event.target.previousElementSibling.value.trim();
     const lowercaseName = roomName.toLowerCase();
     const dashedName = lowercaseName.replace(' ', '-');
@@ -44,49 +45,51 @@ const updateRoom = async (event) => {
     if (roomName) {
         const response = await fetch(`/api/rooms/${roomId}`, {
             method: 'PUT',
-            body: JSON.stringify({ 
+            body: JSON.stringify({
                 name: roomName,
                 dashed_name: dashedName,
                 id: roomId
             }),
             headers: { 'Content-Type': 'application/json' },
         }
-        )};
-        document.location.replace('/');
+        )
+    };
+    document.location.replace('/');
 };
 
 const deleteRoom = async (event) => {
     event.preventDefault();
-    
+
     const roomId = event.target.getAttribute('data-index-number');
     console.log(roomId);
 
     if (roomId) {
         const response = await fetch(`/api/rooms/${roomId}`, {
             method: 'DELETE',
-            body: JSON.stringify({ 
+            body: JSON.stringify({
                 id: roomId
             }),
             headers: { 'Content-Type': 'application/json' },
         }
-        )};
-        document.location.replace('/');
+        )
+    };
+    document.location.replace('/');
 };
 
 const handleSearch = async (event) => {
     const roomId = document.querySelector('.delete-btn').getAttribute('data-index-number');
     console.log(roomId);
-  
+
     event.preventDefault();
-    
+
     const q = document.querySelector('.form-select').value;
-    
+
     if (q) {
         const response = await fetch(`/api/furniture/${q}`)
     }
     document.location.replace(`/api/furniture/${q}`);
-  }
-  
+}
+
 
 document.getElementById('addRoomBtn').addEventListener('click', (event) => {
     showHide(event.target);
@@ -94,12 +97,12 @@ document.getElementById('addRoomBtn').addEventListener('click', (event) => {
 
 document.querySelector('.search-btn').addEventListener('click', (event) => {
     handleSearch(event);
-  });
+});
 
 document.getElementById('submitRoom').addEventListener('click', submitRoom);
 
 
-    
+
 
 document.querySelector('#room-container').addEventListener('click', (event) => {
     if (event.target.matches('.rename-btn')) {
